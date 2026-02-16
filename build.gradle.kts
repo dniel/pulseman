fffc90008fb93e62b345ae3465bee9c68d768862
Daniel Nord <me@danielnord.no>
2026-02-16 19:24:03 +0100

    Bump version to 1.1

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    application
+}
+
+group = "no.dniel"
+version = "1.1"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.repsy.io/mvn/njoh/public")
+    }
+}
+
+dependencies {
+    implementation("no.njoh:pulse-engine:0.13.0")
+}
+
+kotlin {
+    jvmToolchain(23)
+}
+
+val mainClass = "pacman.PacmanGameKt"
+
+application {
+    mainClass.set("pacman.PacmanGameKt")
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    manifest { attributes("Main-Class" to mainClass) }
+}
